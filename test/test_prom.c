a78f62c6f60301eb3314ae4e5d458761bd030913
Emiliano <emiarnedo@gmail.com>
2023-06-04 13:13:09 -0300

    antes de crear el mock

---
+++ test/test_prom.c
@@ -0,0 +1,13 @@
+#include <unity.h>
+#include <prom.h>
+#include "Mocksuma.h"
+
+int auxiliar_suma(int a, int b) {
+    return a + b;
+}
+
+void test_dos_numeros_positivos(void) {
+    suma_fake.custom_fake = auxiliar_suma;
+
+    TEST_ASSERT_EQUAL(3, promedio(2, 4));
+}
